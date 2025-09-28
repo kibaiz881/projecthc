9e6a9aaa580c30004e3d9057b46ee4c238fb4ddb
kibaiz881 <solofo881@gmail.com>
2025-09-28 19:28:25 +0300

    pagination blog

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\PublicationRepository;
+
+final class BlogController extends AbstractController
+{
+    #[Route('/blog/{page}/{ItemPerPage}', name: 'app_blog')]
+    public function index(PublicationRepository $pubrepo, int $page = 1, $ItemPerPage = 10 ): Response
+    {
+        // Paginer les résultats
+        $publications = $pubrepo->createQueryBuilder('p')
+            ->setFirstResult(($page - 1) * $ItemPerPage)
+            ->setMaxResults($ItemPerPage)
+            ->getQuery()
+            ->execute();
+
+        return $this->render('blog/index.html.twig', [
+            'publications' => $publications
+        ]);
+    }
+}
